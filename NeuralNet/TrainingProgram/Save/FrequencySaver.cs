6df2e059678b9fc28b6cd6a4370d9e6f4e811aff
Toxic <markusbolsen@gmail.com>
2024-05-10 06:51:59 +0200

    Change newest-saver to frequency-saver

---
+++ NeuralNet/TrainingProgram/Save/FrequencySaver.cs
@@ -0,0 +1,38 @@
+﻿
+namespace NeuralNet.TrainingProgram.Save;
+
+public sealed class FrequencySaver: INetSaver
+{
+
+    private readonly string path;
+    private readonly int saveFrequency;
+    private readonly bool doOverrideFile;
+
+    private int iterationCount;
+
+    public FrequencySaver(string path, int saveFrequency = 1,  bool doOverrideFile = true) 
+    { 
+        this.path = path;
+        this.saveFrequency = saveFrequency;
+        this.doOverrideFile = doOverrideFile;
+
+        iterationCount = 0;
+    }
+
+    public void Save(INet net)
+    {
+        if(iterationCount % saveFrequency == 0)
+        {
+            if(doOverrideFile)
+            {
+                net.Save(path);
+            }
+            else
+            {
+                net.Save(path + $" ({iterationCount})");
+            }
+        }
+
+        iterationCount++;
+    }
+}
